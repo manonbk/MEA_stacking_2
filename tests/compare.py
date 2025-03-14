a97fe1b7ee9969b7bedfd1d037482fe7e6cb95bc
manonbk <manon.ben-kiran@polytechnique.edu>
2025-03-14 15:38:01 +0100

    nouveaux trucs
    +levenshtein

--- tests/compare.py
+++ tests/compare.py
@@ -1,4 +1,5 @@
 import RNA
+
 # The RNA sequence
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -36,28 +37,39 @@ def similarity (struc1, struc2):
     return count/len(struc1)
 
 
-example_alignement = ["GGAGGAUUAGCUCAGCUGGGAGAGCAUCUGCCUUACAAGCAGAGGG-----------UCGGCGGUUCGAGCCCGUCAUCCUCC",
-"GCCUUCCUAGCUCAG-UGGUAGAGCGCACGGCUUUUAACCGUGUGG-----------UCGUGGGUUCGAUCCCCACGGAAGGC",
-"GCCUUUAUAGCUUAG-UGGUAAAGCGAUAAACUGAAGAUUUAUUUA-----------CAUGUAGUUCGAUUCUCAUUAAGGGC",
-"GCGGAUAUAACUUAGGGGUUAAAGUUGCAGAUUGUGGCUCUGAAAA------------CACGGGUUCGAAUCCCGUUAUUCGC",
-"GGAAAAUU-GAUCAUCGGCAAGAUAAGUUAUUUACUAAAUAAUAGGAUUUAAUAACCUGGUGAGUUCGAAUCUCACAUUUUCC"
-]
+def levenshtein_distance(s1, s2):
+    len_s1, len_s2 = len(s1), len(s2)
+    dp = [[0] * (len_s2 + 1) for _ in range(len_s1 + 1)]
+
+    for i in range(len_s1 + 1):
+        for j in range(len_s2 + 1):
+            if i == 0:
+                dp[i][j] = j  # Cost of inserting j characters
+            elif j == 0:
+                dp[i][j] = i  # Cost of deleting i characters
+            else:
+                cost = 0 if s1[i - 1] == s2[j - 1] else 1
+                dp[i][j] = min(dp[i - 1][j] + 1,      # Deletion
+                            dp[i][j - 1] + 1,      # Insertion
+                            dp[i - 1][j - 1] + cost)  # Substitution
+
+    return (len_s2-dp[len_s1][len_s2])/len_s2
 
 
 #print(similarity("(((((((..((((........))))((((((.......))))))...............(((((.......))))))))))))",
 #"(((((((..((((........))))(((((.........)))))...............(((((.......))))))))))))"))
 
-def similarity_matrix(alignment, gamma=2, delta=1,m=2):
+def similarity_matrix(alignment, dist_function, gamma=2, delta=1,m=2):
     
     structures= consensus_structures(alignment, gamma, delta ,m)
     print(structures)
     matrix=np.zeros((3,3))
     for i in range (3):
         for j in range(i,3):
-            sim_score=similarity(structures[i], structures[j])
+            sim_score=dist_function(structures[i], structures[j])
             matrix[i][j]=sim_score
             matrix[j][i]=sim_score
-    return(matrix)
+    return( structures,matrix)
 
 def plot_similarity_matrix(matrix, method_names):
     plt.figure(figsize=(8, 6))
